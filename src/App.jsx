ac16a1a7cb4409d76a39a173d216f2f32000da94
devsabbirhossain <sabbir2dev@gmail.com>
2024-03-20 15:24:33 +0600

    initial smartgrade

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import StudentList from "./components/StudentList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <StudentList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
